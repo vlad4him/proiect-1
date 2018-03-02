4edc1ce825baa965b97858f57501a6285e493f12
Vlad Dinca <vlad4him@gmail.com>
2018-03-02 15:38:41 +0200

    hex

--- js/java1.js
+++ js/java1.js
@@ -25,6 +25,62 @@
 		});
 
 	
+var counter = document.getElementById('counter'),
+	counterValue = counter.innerHTML,
+	counterInterval = -1,
+	counterBackground = parseInt('FFFFFF', 16);
+
+console.log(counterBackground);
+
+var runner = document.getElementById('runner'),
+	stopper = document.getElementById('stop');
+
+stopper.addEventListener('click', function() {
+
+	clearInterval(counterInterval);
+	counterInterval = -1;
+
+});
+
+runner.addEventListener('click', function() {
+
+
+	if (counterInterval !== -1) return;
+
+
+	counterInterval = setInterval(function() {
+
+		counterValue++;
+
+		counter.innerHTML = counterValue; 
+
+
+		counterBackground = counterBackground + 10000;
+
+		counter.style.backgroundColor = '#'+ counterBackground.toString(16).substr(0, 6);
+		console.log(counterBackground.toString(16));
+
+
+	}, 1000);
+
+
+});
+
+
+
+
+// $('#runner').click(function() {
+//    var amount=parseInt($('#amount').val());
+//    $({c:0}).animate({c:amount}, { 
+//         step: function(now) {
+//             $("#count").val(Math.round(now))
+//         },
+//         duration: 30000,
+//         easing: "linear"        
+//     });
+// })
+
+
 		
 //https://www.w3schools.com/js/pic_bulbon.gif
 //https://www.w3schools.com/js/pic_bulboff.gif
@@ -32,13 +88,3 @@
 
 })(window, document, undefined); 
 
-$('#runner').click(function() {
-   var amount=parseInt($('#amount').val());
-   $({c:0}).animate({c:amount}, { 
-        step: function(now) {
-            $("#count").val(Math.round(now))
-        },
-        duration: 30000,
-        easing: "linear"        
-    });
-})
